bce6b42f057ff0bc678128b04b2d1e72cae576b5
Abel de Bruijn <abeldebruijn@hotmail.com>
2023-01-17 13:42:30 +0100

    Parse other options for applets

--- README.md
+++ README.md
@@ -16,11 +16,17 @@ To run once and see the result:
 jupyter-book build .
 ```
 
-To run with local applets:
+To run with local applets once:
 
 ```bash
 BASE_URL=http://localhost:3000/applet/ jupyter-book build .
 # Or some other port
+```
+
+To run the book with 'hot-reload':
+
+```bash
+watchexec -e py,md -i temp.md -- 'echo -n a >> temp.md && jupyter-book build .'
 ```
 
 ## How to add an applet to an image
@@ -42,3 +48,23 @@ A plane through the point $P$.
 ````
 
 > ⚠️ It is not required to add the `base url` and `/applet`. This is handed with an environment variable name `BASE_URL`. The default base url is set to https://openla.ewi.tudelft.nl/applet/
+
+## Parameters for an applet
+
+Some parameters can be set for an applet. The url parameter is the only required; the rest is optional.
+
+````bash
+```{applet}
+:url: lines_and_planes/normal_equation_plane_origin # Required url
+:title: hello # a string that will be shown as the title of the applet when the aplet is in fullscreen mode
+:width: 100% # the width of the applet
+:height: 500px # the height of the applet
+:background: #ffffff # the background color of the applet
+:autoPlay: true # if the applet should start playing automatically
+:isPerspectiveCamera: false # if the camera should be a perspective or orthographic camera
+:position: 1,1,1 # the position of the camera related to the origin
+:enablePan: false # if the user can pan the camera (right mouse drag on desktop, two finger drag on mobile)
+:distance: 30 # the distance of the camera from the origin for a *perspective* camera. Distance is a linear value, *higher* is further away.
+:zoom: 30 # the distance of the camera from the origin for a *orthographic* camera. Zoom is a logarithmic value, *lower* is further away.
+```
+````
--- _ext/applet.py
+++ _ext/applet.py
@@ -8,18 +8,59 @@ DEFAULT_BASE_URL = 'https://openla.ewi.tudelft.nl/applet/'
 
 class AppletDirective(Directive):
 	option_spec = {
-		'url': directives.unchanged,
-		'description': directives.unchanged
+		'url': directives.unchanged_required,
+		'title': directives.unchanged,
+		'background': directives.unchanged,
+		'autoPlay': directives.unchanged,
+		'position': directives.unchanged,
+		'isPerspectiveCamera': directives.unchanged,
+		'enablePan': directives.unchanged,
+		'distance': directives.unchanged,
+		'zoom': directives.unchanged,
+		'height': directives.unchanged,
+		'width': directives.unchanged,
 	}
 	has_content = False
+
+	def parseOption(self, key: str, params_dict: dict) -> dict:
+		if key in self.options:
+			params_dict[key] = str(self.options[key])
+		
+		return params_dict
+
+	
+	def parseOptions(self) -> dict:
+		params_dict = dict()
+
+		# Guard to check if options is None
+		if self.options is None:
+			return params_dict
+
+		# Keys that are parsed as is
+		parse_keys = ['title', 'background', 'autoPlay', 'position', 'isPerspectiveCamera', 'enablePan', 'distance', 'zoom']
+
+		params_dict = {key: str(self.options[key]) for key in parse_keys if key in self.options}
+
+		# for key in parse_keys:
+		# 	params_dict = self.parseOption(key, params_dict)
+
+		return params_dict
+
 
 	def run(self):
 		base_url = os.environ.get('BASE_URL', DEFAULT_BASE_URL)
 		url = self.options['url']
-		# description = if self.options['description']
-		# name = self.options['name']
+		
+		# TODO: parse width & height
 
-		content = f'<div class="applet">\n  <noscript class="loading-lazy">\n    <iframe src="{base_url}{url}" allow="fullscreen" loading="lazy" frameborder="0"></iframe>\n  </noscript>\n</div>'
+		# title = self.options['title'] if 'title' in self.options else 'Applet'
+
+		params_dict = self.parseOptions()
+
+		# {a: '1', b: '2', c: '1,2,3'} -> 'a=1&b=2&c=1,2,3'
+		params = '&'.join([f'{key}={value}' for key, value in params_dict.items()])
+
+		content = f'<div class="applet">\n  <noscript class="loading-lazy">\n  <iframe src="{base_url}{url}?{params}" allow="fullscreen" loading="lazy" frameborder="0"></iframe>\n  </noscript>\n</div>'
 
 		return [nodes.raw('', content, format='html')]
 
